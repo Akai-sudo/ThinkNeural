0e6befb387dcb844414313754ef52e52ae17b298
Akai-sudo <mohorluka@gmail.com>
2023-03-06 03:38:27 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,112 @@
+import React, {Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Dimensions } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
-export default function App() {
+import { StyleSheet, Text, View, Button, Alert, Pressable } from 'react-native';
+
+/**const Neuron = () => {
+    return <View style={styles.circle} />;
+}
+
+ <View style={styles.fixToText}>
+ <Button
+ title="Continue to the Neural Net!"
+ onPress={() => Alert.alert('Loading neurons...')}
+ />
+ </View>
+
+ <Button style={styles.button} title="Continue >" onPress={() => Alert.alert('Loading neurons...')} />
+
+ **/
+function App(props) {
+  const { onPress, title = 'Continue >' } = props;
   return (
+     // <NavigationContainer>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LinearGradient colors={['rgba(0,150,255,0.9)', 'transparent']} style={styles.linearGradient}>
+
+        <View style={styles.fixToText}>
+          <Text style={styles.mainTitle}>Hello{"\n"}This is a neuron. On it's own it's just a number.</Text>
+        </View>
+
+        <StatusBar style="auto" />
+        <View style={styles.circle}/>
+
+
+        <Pressable style={styles.button} onPress={onPress}>
+          <Text style={styles.text}>{title}</Text>
+        </Pressable>
+
+
+
+      </LinearGradient>
     </View>
+
+     // </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6495ED',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 17,
+    lineHeight: 21,
+    fontWeight: 'normal',
+    letterSpacing: 0.25,
+    color: '#24a0ed',
+  },
+
+  linearGradient: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+  },
+
+  mainTitle: {
+    padding: 30,
+    margin: 20,
+    //shadowOffset: 0.1,
+    shadowRadius: 100,
+    shadowColor: "black",
+    fontSize: 38,
+    textAlign: "center",
+    color: 'white',
+  },
+
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 160,
+    height: 60,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 25,
+    elevation: 3,
+    marginTop: 50,
+    backgroundColor: 'white',
+    //backgroundColor: ['rgba(0,150,255,0.9)', 'transparent'],
+
+  },
+
+  circle: {
+    height: 75,
+    width: 75,
+    marginBottom: 20,
+    blurRadius: 1,
+    borderRadius: 75/2,
+    backgroundColor: 'white',
+  }
 });
+
+
+export default App;
